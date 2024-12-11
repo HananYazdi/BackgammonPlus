42855c5541a2406cb38e480ee82c5046359db05a
OrAbutbul1 <orabutbul1@gmail.com>
2024-12-11 09:37:21 +0200

    add timer

--- src/InfoPanel.java
+++ src/InfoPanel.java
@@ -1,45 +1,53 @@
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class InfoPanel extends JPanel {
-    private Game game;
-    private JLabel rolls;
-    private JLabel activePlayer;
-    private JLabel scores;
+	private Game game;
+	private JLabel rolls;
+	private JLabel activePlayer;
+	private JLabel scores;
+	private JLabel time;
 
-    public InfoPanel(Game game) {
-        this.game = game;
+	public InfoPanel(Game game) {
+		this.game = game;
 
-        activePlayer = new JLabel();
-        rolls = new JLabel();
-        scores = new JLabel();
+		activePlayer = new JLabel();
+		rolls = new JLabel();
+		scores = new JLabel();
+		time = new JLabel();
 
-        add(scores);
-        add(activePlayer);
-        add(rolls);
-    }
+		add(scores);
+		add(activePlayer);
+		add(rolls);
+		add(time);
+	}
 
-    public void updateInfo() {
-        int[] r = game.getRolls();
-        StringBuilder rollText = new StringBuilder("Moves: ");
-        int[] s = game.getScores();
-        StringBuilder scoreText = new StringBuilder("Score: ");
-        
-        for(int score : s){
-            scoreText.append(score);
-            scoreText.append(", ");
-        }
+	public void updateInfo() {
+		int[] r = game.getRolls();
+		StringBuilder rollText = new StringBuilder("Moves: ");
+		int[] s = game.getScores();
+		StringBuilder scoreText = new StringBuilder("Score: ");
+		String t = game.updateTimerDisplay();
+		StringBuilder timeText = new StringBuilder("Time: ");
+		timeText.append(t);
 
-        for (int roll : r){
-            rollText.append(roll);
-            rollText.append(", ");
-        }
-        rollText.setLength(rollText.length() - 2);
-        scoreText.setLength(scoreText.length() - 2);
+		for (int score : s) {
+			scoreText.append(score);
+			scoreText.append(", ");
+		}
 
-        scores.setText(scoreText.toString());
-        rolls.setText(rollText.toString());
-        activePlayer.setText(game.getActivePlayer().getName());
+		for (int roll : r) {
+			rollText.append(roll);
+			rollText.append(", ");
+		}
+		rollText.setLength(rollText.length() - 2);
+		scoreText.setLength(scoreText.length() - 2);
 
-        repaint();
-    }
+		scores.setText(scoreText.toString());
+		rolls.setText(rollText.toString());
+		time.setText(timeText.toString());
+		activePlayer.setText(game.getActivePlayer().getName());
+
+		repaint();
+	}
 }
